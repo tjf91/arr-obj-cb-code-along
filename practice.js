b29ae777e4d00b809f886e73ceb251b167efb7c0
Andrew Westenskow <andrew.westenskow@gmail.com>
2020-10-26 18:04:17 -0400

    First pass

---
+++ practice.js
@@ -0,0 +1,328 @@
+//////////////////PROBLEM 1////////////////////
+
+// Modify the function below to return the first item of the array that is being passed in
+
+function first(arr) {
+  //Code here
+}
+
+//////////////////PROBLEM 2////////////////////
+
+// Modify the function below to return the length of the array that is being passed in
+
+function length(arr) {
+  //Code here
+}
+
+//////////////////PROBLEM 3////////////////////
+
+// Modify the function below to return the last item of the array that is being passed in
+
+function last(arr) {
+  //Code here
+}
+
+//////////////////PROBLEM 4////////////////////
+
+// Modify the function below to add the passed in number to the end of the passed in array
+
+function addItem(array, number) {
+  //Code here
+  return array
+}
+
+//////////////////PROBLEM 5////////////////////
+
+// Modify the function below to remove the last item of the array being passed in
+
+function removeLast(array) {
+  //Code here
+  return array
+}
+
+//////////////////PROBLEM 6////////////////////
+
+// Modify the function below to remove the first item of the array being passed in
+
+function removeFirst(array) {
+  //Code here
+  return array
+}
+
+//////////////////PROBLEM 7////////////////////
+
+// Modify the given for loop to push i into the numbers array. If done correctly numbers should equal: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
+
+function maker() {
+  var numbers = []
+  for (var i = 0; i <= 25; i++) {
+    //Code here
+  }
+
+  return numbers
+}
+
+//////////////////PROBLEM 8////////////////////
+
+// Fill the numbers array with numbers 0-31.
+
+function count31() {
+  var numbers = []
+
+  //Code here
+
+  return numbers
+}
+
+//////////////////PROBLEM 9////////////////////
+
+// Using the given for loop, fill the newArray with the passed in array's elements in reverse order.
+// For example:
+// backWards( [0,1,2,3] ); -> [3,2,1,0]
+// backWards( [ true, false ] ); -> [ false, true ]
+// backWards( [ 'first', 'last' ] ); -> [ 'last', 'first' ]
+
+function backWards(arr) {
+  var newArray = []
+
+  for (var i = arr.length - 1; i >= 0; i--) {
+    // code here
+  }
+
+  return newArray
+}
+
+//////////////////PROBLEM 10////////////////////
+
+// Modify the function below to search the passed in array for the passed in value. If the array contains the value, return true. Otherwise return false.
+// For example:
+// findInArray([0,1,2,3], 3); -> true
+// findInArray([0,1,2,3], 4); -> false
+
+function findInArray(arr, value) {
+  // code here
+}
+
+//////////////////PROBLEM 11////////////////////
+
+// Modify the given for loop to add ten to each element in the passed in array and then push the sum into the newArr array. You can assume that all items in the array will be numbers.
+// For example:
+// addTen([10,20,30]); -> [20,30,40]
+// addTen([1,2,3,4]); -> [11,12,13,14]
+
+function addTen(arr) {
+  var newArr = []
+
+  for (var i = 0; i < arr.length; i++) {
+    // code here
+  }
+
+  return newArr
+}
+
+//////////////////PROBLEM 12////////////////////
+
+// Modify the function below to return the firstName property of the person object.  Use dot notation
+
+function personName() {
+  var person = {
+    firstName: 'sally',
+    lastName: 'smith',
+    age: 29,
+    location: 'Orem, UT',
+  }
+  return // Code here
+}
+
+//////////////////PROBLEM 13////////////////////
+
+// Modify the function below to return the location property of the person object.  Use bracket notation
+
+function personLocation() {
+  var person = {
+    firstname: 'sally',
+    lastname: 'smith',
+    age: 29,
+    location: 'Orem, UT',
+  }
+  return // Code Here
+}
+
+//////////////////PROBLEM 14////////////////////
+
+//Use dot notation to add a new property to the object backpack called frontPocket with the value equal to "compass".
+
+const backpack = {}
+
+// Code Here
+
+//////////////////PROBLEM 15////////////////////
+
+//Use bracket notation to add a material property to the box object.  Set it's value equal to the string 'cardboard'
+
+const box = {}
+
+// Code Here
+
+//////////////////PROBLEM 16////////////////////
+
+//Create a variable called name.  Assign it the value of the firstName property using dot notation.
+
+const person = {}
+
+person['firstName'] = 'sally'
+
+//code here
+
+//////////////////PROBLEM 17////////////////////
+
+// In the function updateUser, change the user parameter's name to Ryan, change the value of pwHash to superSafe and change the value of username to ryan2020. After you have updated all the values return the object.
+
+// The existing user looks like this:
+// var someUser = {
+//   name: 'Sally Rally',
+//   pwHash: 'U+Ldlngx2BYQk',
+//   username: 'SallyRally801'
+// };
+
+function updateUser(user) {
+  // Code Here
+}
+
+//////////////////PROBLEM 18////////////////////
+
+//Inside the function updateEmail, update the email property of the passed in object to be the value of the parameter str. Return the updated object.
+
+function updateEmail(obj, str) {
+  // Code here
+}
+
+//////////////////PROBLEM 19////////////////////
+
+// Write a function called isOldEnough that takes a person obj and checks the age property to see if the person is old enough to enter the club.  If they are 21 or older return true else return false.
+
+// Code here
+
+//////////////////PROBLEM 20////////////////////
+
+//Create a function called addRole that takes in a user object as the first parameter and a string as the second parameter.  The string will represent the user's new role in the system (i.e. admin, creator, editor, visitor). Create a new property on the user object called "role" and assign the passed in string to it, then return the updated object.
+
+// Code here
+
+///////////////////////////////////////////////////////
+
+/*
+* Ternary Operator
+* Ternary operators are similar to if-statements. Below is the basic structure of a ternary statement.
+
+! condition ? firstExpression : secondExpression
+* If condition evaluates to true, firstExpression is executed. What do you think will happen if condition evaluates to false?
+
+* Our checkAge function accepts one parameter, and declares a variable that will have one of two values depending on that parameter.
+
+! Examine -- don't run -- the code.
+* What is the condition being tested in checkAge?
+* If this condition evaluates to true, what will happen? If it evaluates to false?
+* Determine the value of oldEnough on lines 10-12
+* Run the code to see if you're correct
+*/
+function checkAge(age) {
+  var oldEnough = age >= 21 ? 'Yup' : 'Nope'
+  // oldEnough can only be one of two values.
+  // It can be 'Yup' or 'Nope',
+  // and that depends on the result of the 'age >= 21' condition.
+  // What will the value of oldEnough be?
+  console.log(oldEnough)
+}
+
+//////////////////PROBLEM 21////////////////////
+
+// Rewrite the existing if statement as a ternary.
+
+function schoolStatus(status) {
+  var parentsMessage
+
+  // if (status == 'Good') {
+  //   parentsMessage = 'Wow, great job kiddo!'
+  // } else {
+  //   parentsMessage = 'No more video games!'
+  // }
+
+  return parentsMessage
+}
+
+//////////////////PROBLEM 22////////////////////
+
+// Complete the messageBasedOnAge function by using a ternary statement to return a string based on the age parameter.
+
+// If age is less than 18, return: Not quite old enough, sorry.
+// If age is 18, return: Congrats on being an adult!
+// If age is greater than 18, return: Somebody is really getting up there, huh?
+
+function messageBasedOnAge(age) {
+  //code here
+}
+
+//////////////////PROBLEM 23////////////////////
+
+// Create a function called outerFn, that takes in a callback as a parameter, and then returns that callback invoked.
+// To see it working, invoke outerFn at the bottom, passing in the InnerFn as the callback. You should now see "The innerFn is a callback!" in the console.
+
+// Create function here
+
+// ===== DO NOT TOUCH CODE BELOW THIS LINE ===== //
+function innerFn() {
+  return 'The innerFn is a callback!'
+}
+// ===== DO NOT TOUCH CODE ABOVE THIS LINE ===== //
+
+// Invoke function here
+
+//////////////////PROBLEM 24////////////////////
+
+// Create a function called fullName, that takes in three parameters: firstName, lastName, and a callback.
+// The fullName function should return the callback, passing in firstName and lastName as parameters.
+// To test, invoke fullName with your first name, last name and the welcomeMessage function as arguments.
+
+// Create function fullName here
+
+// ===== DO NOT TOUCH CODE BELOW THIS LINE ===== //
+function welcomeMessage(first, last) {
+  return `Welcome to DevMountain, ${first} ${last}!`
+}
+// ===== DO NOT TOUCH CODE ABOVE THIS LINE ===== //
+
+// Invoke fullName below
+
+//////////////////PROBLEM 25////////////////////
+
+// Do not edit the code below.
+let age = 'TBD'
+// Do not edit the code above.
+
+// Create a function called canDrink that will take in three parameters. The first parameter will be an number representing an age. The second and third parameters will be callbacks. The functions drinkSoda and drinkAlcohol are examples of callbacks that could be passed into your function.
+
+// If the age is below 21, return the invocation of the first callback
+// If the age is 21 or greater, return the invocation of the second callback
+
+// ===== DO NOT TOUCH CODE BELOW THIS LINE ===== //
+function drinkSoda() {
+  return 'Come back when you are legally allowed a drink!'
+}
+
+function drinkAlcohol() {
+  return 'Drink responsibly!'
+}
+
+// ===== DO NOT TOUCH CODE ABOVE THIS LINE ===== //
+
+// Create function canDrink here
+
+//////////////////PROBLEM 26////////////////////
+
+// Write two functions, one called add and one called multiply, that each takes in two numbers and returns the appropriate new value.
+
+// Write a function called math that takes in two numbers, and a callback 'operator' as parameters.
+// This function should return a operator invoked with the appropriate arguments.
+
+//Code here
---
+++ test/spec/practiceSpec.js
@@ -0,0 +1,236 @@
+//Test Suite
+
+describe('JavaScript2 Code Along', function () {
+  describe('Problem 1 - first', function () {
+    const arr = [1, 2, 3]
+    it('Returns the first item of the array', () => {
+      expect(first(arr)).toBe(1)
+    })
+  })
+  describe('Problem 2 - length', function () {
+    const arr = [1, 2, 3]
+    it('Returns the length of the array', () => {
+      expect(length(arr)).toBe(3)
+    })
+  })
+  describe('Problem 3 - last', function () {
+    const arr = [1, 2, 3]
+    it('Returns the last item in the array', () => {
+      expect(last(arr)).toBe(3)
+    })
+  })
+  describe('Problem 4 - add item to array', function () {
+    const arr = [1, 2, 3]
+    it('Adds given item to given array', () => {
+      expect(addItem(arr, 4)).toEqual([1, 2, 3, 4])
+    })
+  })
+  describe('Problem 5 - remove last item from array', function () {
+    it('Removes the last item from the given array', () => {
+      const arr = [1, 2, 3]
+      expect(removeLast(arr)).toEqual([1, 2])
+    })
+  })
+  describe('Problem 6 - remove first item from array', function () {
+    const arr = [1, 2, 3]
+    expect(removeFirst(arr)).toEqual([2, 3])
+  })
+  describe('Problem 7 - push 1 - 25 to the given array', function () {
+    const arr = [...Array(26).keys()]
+    it('Correctly populates the array', () => {
+      expect(maker()).toEqual(arr)
+    })
+  })
+  describe('Problem 8 - 1 - 31 in array', function () {
+    const arr = [...Array(32).keys()]
+    it('Correctly populates array', function () {
+      expect(count31()).toEqual(arr)
+    })
+  })
+  describe('Problem 9 - reverse array', function () {
+    const arr1 = [0, 1, 2, 3]
+    const arr2 = [true, false]
+    const arr3 = ['first', 'last']
+
+    it('Correctly reverses first array', () => {
+      expect(backWards(arr1)).toEqual(arr1.reverse())
+    })
+    it('Correctly reverses second array', () => {
+      expect(backWards(arr2)).toEqual(arr2.reverse())
+    })
+    it('Correctly reverses third array', () => {
+      expect(backWards(arr3)).toEqual(arr3.reverse())
+    })
+  })
+
+  describe('Problem 10 - findInArray', function () {
+    const arr = [1, 2, 3, 4]
+
+    it('Correctly finds item', () => {
+      expect(findInArray(arr, 4)).toBe(true)
+    })
+    it('Correctly says item could not be found', () => {
+      expect(findInArray(arr, 5)).toBe(false)
+    })
+  })
+  describe('Problem 11 - add 10', function () {
+    const arr = [10, 20, 30]
+    it('should add 10 to each item', function () {
+      expect(addTen(arr)).toEqual([20, 30, 40])
+    })
+  })
+  describe('Problem 12 - dot notation', function () {
+    it('returns the correct property', () => {
+      expect(personName()).toBe('sally')
+    })
+  })
+  describe('Problem 13 - bracket notation', function () {
+    it('Returns the correct property', () => {
+      expect(personLocation()).toBe('Orem, UT')
+    })
+  })
+  describe('Problem 14 - add new property to object using dot notation', function () {
+    it('has the correct property', () => {
+      expect(backpack.frontPocket).toBe('compass')
+    })
+  })
+  describe('Problem 15 - add new property to object using bracket notation', function () {
+    it('has the correct property', function () {
+      expect(box.material).toBe('cardboard')
+    })
+  })
+  describe('Problem 16 - assign property to variable', function () {
+    it('name variable exists', function () {
+      expect(name).toBeDefined()
+    })
+    it('name variable has correct value', function () {
+      expect(name).toBe('sally')
+    })
+  })
+  describe('Problem 17 - update user', function () {
+    const someUser = {
+      name: 'Sally Rally',
+      pwHash: 'U+Ldlngx2BYQk',
+      username: 'SallyRally801',
+    }
+    updateUser(someUser)
+    it('correctly modifies name property', function () {
+      expect(someUser.name).toBe('Ryan')
+    })
+    it('correctly modifies pwHash property', function () {
+      expect(someUser.pwHash).toBe('superSafe')
+    })
+    it('correctly modifies username property', function () {
+      expect(someUser.username).toBe('ryan2020')
+    })
+  })
+  describe('Problem 18 - update email property', function () {
+    const person = { email: 'oldValue' }
+    updateEmail(person, 'newValue')
+    it('correctly updates email property', function () {
+      expect(person.email).toBe('newValue')
+    })
+  })
+  describe('Problem 19 - is old enough', function () {
+    it('isOldEnough should exist', function () {
+      expect(isOldEnough).toBeDefined()
+      expect(typeof isOldEnough).toBe('function')
+    })
+    it('Returns correct answer', function () {
+      let result = isOldEnough(16)
+      expect(result).toBe(false)
+    })
+    it('Returns correct answer', function () {
+      let result = isOldEnough(26)
+      expect(result).toBe(true)
+    })
+  })
+  describe('Problem 20 - addRole', function () {
+    const obj = { name: 'Andrew' }
+    it('createName should exist', function () {
+      expect(addRole).toBeDefined()
+      expect(typeof addRole).toBe('function')
+    })
+    it('should add property to object', function () {
+      addRole(obj, 'admin')
+      expect(obj.role).toBe('admin')
+    })
+  })
+  describe('Problem 21 - schoolStatus', function () {
+    it('Should return correct value', () => {
+      expect(schoolStatus('Good')).toBe('Wow, great job kiddo!')
+    })
+    it('Should return correct value', () => {
+      expect(schoolStatus('Bad')).toBe('No more video games')
+    })
+  })
+  describe('Problem 22 - messageBasedOnAge', function () {
+    it('Should return correct value', () => {
+      expect(messageBasedOnAge(15)).toBe('Not quite old enough, sorry.')
+    })
+    it('Should return correct value', () => {
+      expect(messageBasedOnAge(18)).toBe('Congrats on being an adult!')
+    })
+    it('Should return correct value', () => {
+      expect(messageBasedOnAge(25)).toBe(
+        'Somebody is really getting up there, huh?'
+      )
+    })
+  })
+  describe('Problem 23 - callbacks 1', function () {
+    it('outerFn should exist', function () {
+      expect(outerFn).toBeDefined()
+      expect(typeof outerFn).toBe('function')
+    })
+
+    it('Correctly invokes inner function', () => {
+      expect(outerFn()).toBe('The innerFn is a callback!')
+    })
+  })
+  describe('Problem 24 - callbacks 2', function () {
+    it('fullName should exist', function () {
+      expect(fullName).toBeDefined()
+      expect(typeof fullName).toBe('function')
+    })
+
+    it('Correctly executes callback', () => {
+      expect(fullName('First', 'Last', welcomeMessage)).toBe(
+        'Welcome to DevMountain, First Last!'
+      )
+    })
+  })
+  describe('Problem 25 - canDrink', function () {
+    it(`canDrink should exist`, function () {
+      expect(canDrink).toBeDefined()
+      expect(typeof canDrink).toBe('function')
+    })
+    it('returns correct value', () => {
+      expect(canDrink(12, drinkSoda, drinkAlcohol)).toBe(
+        'Come back when you are legally allowed a drink!'
+      )
+    })
+    it('returns correct value', () => {
+      expect(canDrink(22, drinkSoda, drinkAlcohol)).toBe('Drink responsibly!')
+    })
+  })
+  describe('Problem 26 - calculator', function () {
+    it(`add should exist`, function () {
+      expect(add).toBeDefined()
+      expect(typeof add).toBe('function')
+    })
+    it(`multiply should exist`, function () {
+      expect(multiply).toBeDefined()
+      expect(typeof multiply).toBe('function')
+    })
+    it(`math should exist`, function () {
+      expect(math).toBeDefined()
+      expect(typeof math).toBe('function')
+    })
+    it('returns correct value', () => {
+      expect(math(2, 5, add)).toBe(7)
+    })
+    it('returns correct value', () => {
+      expect(math(2, 5, multiply)).toBe(10)
+    })
+  })
+})
